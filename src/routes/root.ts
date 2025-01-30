6f7f27bd67d717ec0ee5cb7ec0e8ab355cf96ff7
michelp76 <michel.puissant@inextenso.fr>
2025-01-30 16:12:26 +0100

    Purge table dans QTEMP

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,18 +1,24 @@
 import express from "express";
+import { StringifyOptions } from "querystring";
 import db from "../db";
 
 const root = express.Router();
 
 root.get("/descTable/:table", async (req, res) => {
-  try {
-    // TODO: aller piocher aussi bien dans QSQLSRC (tout autant que QDDSSRC)
+  // TODO: aller piocher aussi bien dans QSQLSRC (tout autant que QDDSSRC)
+  const reqTable: String = req.params.table;
 
+  try {
     // Créer un alias pour accéder au membre
-    const sqlCreate: string = `CREATE OR REPLACE ALIAS QTEMP.${req.params.table} FOR netpaisrc.qddssrc (${req.params.table})`;
+    const sqlCreate: string = `CREATE OR REPLACE ALIAS QTEMP.${reqTable} FOR netpaisrc.qddssrc (${reqTable})`;
     const resAlias = await db.query(sqlCreate);
 
     // Affichage final
-    const result = await db.query(`SELECT * FROM QTEMP.${req.params.table}`);
+    const result = await db.query(`SELECT * FROM QTEMP.${reqTable}`);
+
+    // Ménage (à faire cf. https://www.rpgpgm.com/2014/09/accessing-multiple-member-files-in-sql.html)
+    const sqlDrop: string = `DROP ALIAS QTEMP.${reqTable}`;
+    const resDrop = await db.query(sqlDrop);
 
     if (result.length > 0) {
       // --
@@ -21,13 +27,11 @@ root.get("/descTable/:table", async (req, res) => {
         result,
       });
     } else {
-      res.status(404).json({ error: "no results" });
+      res.status(404).json({ error: "pas de description de fichier" });
     }
   } catch (error) {
     console.error(error);
-    res
-      .status(404)
-      .json({ error: ` fichier inexistant : ${req.params.table}` });
+    res.status(404).json({ error: ` fichier inexistant : ${reqTable}` });
   }
 });
 
