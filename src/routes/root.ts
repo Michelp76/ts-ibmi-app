fb3e5e5d9cbb6a53e310b722bf5acdf47ab1f1e5
michelp76 <michel.puissant@inextenso.fr>
2025-01-30 15:22:18 +0100

    gestion des erreurs améliorée

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -4,19 +4,29 @@ import db from "../db";
 const root = express.Router();
 
 root.get("/descTable/:table", async (req, res) => {
-  // Créer un alias pour accéder au membre
-  const sql = `CREATE ALIAS QTEMP.${req.params.table} FOR netpaisrc.qddssrc (${req.params.table})`;
-  const resAlias = await db.query(sql);
-  const result = await db.query(`SELECT * FROM QTEMP.${req.params.table}`);
+  try {
+    // Drop existant
+    // const sqlDrop: string = `DROP ALIAS QTEMP.${req.params.table}`;
+    // const resDrop = await db.query(sqlDrop);
 
-  if (result.length > 0) {
-    // --
-    res.json({
-      length: result.length,
-      result,
-    });
-  } else {
-    res.status(404).json({ error: "no results" });
+    // Créer un alias pour accéder au membre
+    const sqlCreate: string = `CREATE OR REPLACE ALIAS QTEMP.${req.params.table} FOR netpaisrc.qddssrc (${req.params.table})`;
+    const resAlias = await db.query(sqlCreate);
+
+    // Affichage final
+    const result = await db.query(`SELECT * FROM QTEMP.${req.params.table}`);
+
+    if (result.length > 0) {
+      // --
+      res.json({
+        length: result.length,
+        result,
+      });
+    } else {
+      res.status(404).json({ error: "no results" });
+    }
+  } catch (error) {
+    console.error(error);
   }
 });
 
