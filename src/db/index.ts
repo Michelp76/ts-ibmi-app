b7dc5521decf6f385d899f31546f069db97fadf8
michelp76 <michel.puissant@inextenso.fr>
2025-02-13 16:54:08 +0100

    Affiche les Logs système (DUMP) et source du programme planté

--- src/db/index.ts
+++ src/db/index.ts
@@ -15,17 +15,16 @@ export default class {
     return this.pool.query(statement, bindingsValues) as Promise<T[]>;
   }
   */
-  static async query<T>(statement: string, bindingsValues: (number|string)[] = []) {
+  static async query<T>(statement: string, bindingsValues: (number | string)[] = []) {
     return this.pool.query(statement, bindingsValues);
   }
 
-  static async callProcedure<T>(catalog: string|null, library: string, procedure: string, bindingsValues: (number|string)[] = []) {
+  static async callProcedure<T>(catalog: string | null, library: string, procedure: string, bindingsValues: (number | string)[] = []) {
     const connection = await this.pool.connect();
     return connection.callProcedure(catalog, library, procedure, bindingsValues);
   }
 }
 
-// Ne fonctionne pas comme ça :/
 export const connectionString = [
   `DRIVER={Client Access ODBC Driver (32-bit)}`,
   `SYSTEM=${process.env.DB_HOST}`,
@@ -33,4 +32,5 @@ export const connectionString = [
   `pwd=${process.env.DB_PASSWORD}`,
   `Naming=1`,
   `DBQ=,${process.env[`DB_DBQ`] ? process.env[`DB_DBQ`] : `*USRLIBL`}`,
+  `CCSID=1208`
 ].join(`;`);
--- src/routes/root.ts
+++ src/routes/root.ts
@@ -4,25 +4,50 @@ import db from "../db";
 const root = express.Router();
 
 // Créer un alias pour accéder au membre
-function createAlias(schema: String, rqTable: String): string {
-  return `CREATE OR REPLACE ALIAS QTEMP.${rqTable} FOR netpaisrc.${schema} (${rqTable})`;
+function createAlias(schema: string, rqObject: string): string {
+  return `CREATE OR REPLACE ALIAS QTEMP.${rqObject} FOR netpaisrc.${schema} (${rqObject})`;
 }
 
 // Créer un alias pour accéder au membre
-function selectAlias(rqTable: String): string {
-  return `SELECT * FROM QTEMP.${rqTable}`;
+function selectAlias(rqObject: string): string {
+  return `SELECT * FROM QTEMP.${rqObject}`;
 }
 
 // Suppression nécessaire (idéalement)
-function dropAlias(rqTable: String) {
-  return `DROP ALIAS QTEMP.${rqTable}`;
+function dropAlias(rqObject: string) {
+  return `DROP ALIAS QTEMP.${rqObject}`;
+}
+
+// Recherche à quelle table appartient la colonne (reqZone) demandée
+function searchSysColumn(rqZone: string): string {
+  return `SELECT TABLE_NAME FROM QSYS2.SYSCOLUMNS \
+          WHERE TABLE_OWNER = 'GRDEVSPAF' \
+          AND TABLE_SCHEMA = '${process.env.DB_DBQ}' \
+          AND COLUMN_NAME = '${rqZone}'`;
+}
+
+// Interroge les 'spool files' avec les dumps AS400
+function searchSpooledFiles(
+  jobName: string,
+  spoolFile: string,
+  spFileNum: number = 0,
+  searchString: string = ""
+): string {
+  let query: string = `SELECT SPOOLED_DATA FROM TABLE(SYSTOOLS.SPOOLED_FILE_DATA(JOB_NAME => '${jobName}' \
+                     , SPOOLED_FILE_NAME => '${spoolFile}' \
+                    ${spFileNum > 0 ? ", SPOOLED_FILE_NUMBER => 1" : ""} )) `;
+  if (searchString !== "")
+    query += `WHERE UPPER(SPOOLED_DATA) LIKE '%${searchString}%'`;
+  return query;
 }
 
 // Retourne une DDS (description) de fichier
-async function descTable(rqTable: String): Promise<any> {
+async function descObject(rqObject: string): Promise<any> {
   enum FileType {
     dds = "qddssrc",
     ddl = "qsqlsrc",
+    rpg = "qrpglesrc",
+    clp = "qclpsrc",
   }
 
   try {
@@ -30,24 +55,22 @@ async function descTable(rqTable: String): Promise<any> {
 
     let key: keyof typeof FileType;
     for (key in FileType) {
-      const sqlCreate: string = createAlias(FileType[key], rqTable);
+      const sqlCreate: string = createAlias(FileType[key], rqObject);
       await db.query(sqlCreate);
 
       try {
-        const sqlSel: string = selectAlias(rqTable);
+        const sqlSel: string = selectAlias(rqObject);
         dspSel = await db.query(sqlSel);
       } catch {
-        // Si la requête est invalide, on atterit ici
-        console.log(
-          `requête invalide ou objet '${rqTable}' requêté non trouvé`
-        );
+        // Si la requête est invalide, on atterrit ici
+        console.log(`requête invalide ou objet '${rqObject}' requêté non trouvé`);
       } finally {
         // Ménage
-        const sqlDrop: string = dropAlias(rqTable);
+        const sqlDrop: string = dropAlias(rqObject);
         await db.query(sqlDrop);
 
         if (dspSel) {
-          break; // Sortie prématurée si dds trouvé dans qddssrc
+          break; // Sortie prématurée si objet
         }
       }
     }
@@ -57,19 +80,11 @@ async function descTable(rqTable: String): Promise<any> {
   }
 }
 
-// Recherche à quelle table appartient la colonne (reqZone) demandée
-function searchSysColumn(rqZone: String): string {
-  return `SELECT TABLE_NAME FROM QSYS2.SYSCOLUMNS \
-          WHERE TABLE_OWNER = 'GRDEVSPAF' \
-          AND TABLE_SCHEMA = '${process.env.DB_DBQ}' \
-          AND COLUMN_NAME = '${rqZone}'`;
-}
-
 // Définition de zones pour une table/fichier
-root.get("/descTable/:table", async (req, res) => {
+root.get("/descObject/:table", async (req, res) => {
   const reqTable: string = req.params.table;
 
-  const result = await descTable(reqTable);
+  const result = await descObject(reqTable);
   if (result.length > 0) {
     // --
     res.json({
@@ -94,7 +109,7 @@ root.get("/searchTables/:zone", async (req, res) => {
   if (result.length > 0) {
     for (let i = 0; i < result.length; i++) {
       let srcTable: string = Object.values(result[i] as string)[0];
-      resTable.push(await descTable(srcTable));
+      resTable.push(await descObject(srcTable));
     }
     if (resTable.length > 0) {
       // --
@@ -107,5 +122,81 @@ root.get("/searchTables/:zone", async (req, res) => {
     }
   }
 });
+
+function findInfoSpl(
+  jobLogFile: string[],
+  keyWordBegin: string,
+  keyWordEnd: string
+) {
+  let foundStr: any = jobLogFile.find((e: any) => e.SPOOLED_DATA.includes(keyWordBegin)) as any;
+  if (foundStr !== "") {
+    const beginPos: number = foundStr.SPOOLED_DATA.indexOf(keyWordBegin) + keyWordBegin.length;
+    const endPos: number = foundStr.SPOOLED_DATA.indexOf(keyWordEnd);
+    foundStr = foundStr.SPOOLED_DATA.substring(beginPos, endPos);
+  }
+  return foundStr.toString().trim();
+}
+
+// Affiche la "job log" d'un travail (depuis les "spool files")
+root.get(
+  "/searchJobLog/:jobNum/:jobUser/:jobName/:searchStr?",
+  async (req, res) => {
+    const jobNum: string = req.params["jobNum"];
+    const jobUser: string = req.params["jobUser"];
+    const jobName: string = req.params["jobName"];
+    const concatJob: string = [jobNum, jobUser, jobName].join(`/`);
+    const searchStr: string = req.params["searchStr"] ? req.params["searchStr"] : "";
+    let targetProg, targetLine: string;
+    // Constantes ----------------------------
+    const cERR_PROG_BEGIN = "unmonitored by ";
+    const cERR_PROG_END = " at";
+    const cERR_LINE_BEGIN = " statement";
+    const cERR_LINE_END = ", instruction";
+    let resFinal: object[] = [];
+
+    try {
+      // QPJOBLOG: synthèse des erreurs
+      const queryJobLog = await searchSpooledFiles(concatJob, "QPJOBLOG");
+      const resJobLog: string[] = await db.query(queryJobLog) as string[];
+      if (resJobLog.length > 0) {
+        // Récupère la ligne dans le source en erreur
+        targetProg = findInfoSpl(resJobLog, cERR_PROG_BEGIN, cERR_PROG_END);
+        targetLine = findInfoSpl(resJobLog, cERR_LINE_BEGIN, cERR_LINE_END);
+        // Objet final renvoyé
+        resFinal.push(resJobLog);
+      }
+
+      // QPPGMDMP: Dump de toutes les variables
+      if (searchStr !== "") {
+        const queryDump = await searchSpooledFiles(concatJob, "QPPGMDMP", 1, searchStr);
+        const resDump = await db.query(queryDump);
+        if (resDump.length > 0) {
+          // Objet final renvoyé          
+          resFinal.push(resDump);
+        }
+      }
+
+      // Interroge le source + montre ligne en erreur
+      const result = await descObject(targetProg);
+      if (result.length > 0) {
+        // Objet final renvoyé        
+        resFinal.push(result)
+      }
+
+      if (resFinal.length > 0) {
+        // --
+        res.json({
+          length: resFinal.length,
+          resFinal,
+        });
+      } else {
+        res.status(404).json({ error: `jobName '${concatJob}' non trouvé` });
+      }
+    } catch (error) {
+      // Si la requête est invalide, on atterit ici
+      console.log(error);
+    }
+  }
+);
 
 export default root;
