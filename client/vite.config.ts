d8e6d978cb9e2f806e2523bcbef69dd91a57b971
michelp76 <michel.puissant@inextenso.fr>
2025-03-07 16:42:17 +0100

    Ajout "reactjs-vite-tailwindcss-boilerplate" template

---
+++ client/vite.config.ts
@@ -0,0 +1,27 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config https://vitest.dev/config
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: '.vitest/setup',
+    include: ['**/test.{ts,tsx}']
+  },
+  server: {
+    // port: 3000,
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+})
