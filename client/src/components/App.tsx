c63f49f96b337ab4bb15b78bc8c5393fbce8934d
michelp76 <michel.puissant@inextenso.fr>
2025-03-18 14:23:57 +0100

    introduction de react-highlight pour gérer highlight.js

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -2,6 +2,8 @@
 // import logo from 'assets/logo.svg'
 import { useState, useEffect } from 'react'
 import SearchBox from 'components/Header'
+import 'highlight.js/styles/github.css'
+import Highlight from 'react-highlight'
 
 const App = () => {
   // LOCAL STATES
@@ -37,14 +39,24 @@ const App = () => {
       </div>
       {objToInspect !== '' && (
         // balise <pre><code> pour highlight.js
-        <pre>
-          <code className="language-plaintext relative mx-auto max-w-4xl flex flex-col gap-3 h-dvh overflow-y-auto break-all bg-base-200 p-6 pt-20 text-xs shadow-md text-gray-700 bg-gray-100">
+        <div className="pt-20">
+
+          <Highlight className="relative mx-auto max-w-4xl flex flex-col gap-3 h-dvh overflow-y-auto break-all bg-base-200 p-7 text-xs shadow-md text-gray-700">
             {logsAS400 &&
               logsAS400.map((log, index) => {
                 return <p key={index}>{log['SRCDTA']}</p>
               })}
-          </code>
-        </pre>
+          </Highlight>
+
+          {/* <pre>
+            <code className="language-plaintext relative mx-auto max-w-4xl flex flex-col gap-3 h-dvh overflow-y-auto break-all bg-base-200 p-6 pt-20 text-xs shadow-md text-gray-700 bg-gray-100">
+              {logsAS400 &&
+                logsAS400.map((log, index) => {
+                  return <p key={index}>{log['SRCDTA']}</p>
+                })}
+            </code>
+          </pre> */}
+        </div>
       )}
     </>
   )
